ed0c7eed82e12ffad7c2e00a86afa04a09c1c0fe
Trofel <113066307+lefortnuno@users.noreply.github.com>
2025-06-11 03:13:51 +0100

    Time: 99 ms (79.71%), Space: 8.6 MB (59.63%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+int* twoSum(int* t, int n, int x, int* r) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (t[i] + t[j] == x) {
+                int* res = (int*)malloc(2 * sizeof(int));
+                res[0] = i;
+                res[1] = j;
+                *r = 2;
+                return res;
+            }
+        }
+    }
+    *r = 0;
+    return NULL;
+}
